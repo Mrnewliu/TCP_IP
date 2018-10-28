2310937e72751ee8041c29ae01b32c911d684585
Mrnewliu <15271849112@163.com>
2018-10-29 00:45:56 +0800

    df

---
+++ 10/echo_mpserv.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include<unistd.h>
+#include<signal.h>
+#include<sys/wait.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+
+#define BUF_SIZE 30
+
+void read_childproc(int sig)
+{
+    
+
+}
+
+int main(int argc,char* argv[])
+{
+    
+
+    return 0;
+}
+
