9884486dd79d020b224b74a2149b1b08086db489
Mrnewliu <15271849112@163.com>
2018-11-01 23:38:57 +0800

    第十二章

---
+++ 12/echo_selectserv.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<sys/time.h>
+#include<sys/select.h>
+
+
+#define BUF_SIZE 30
+
+int main(int argc,char* argv[])
+{
+    
+    return 0;
+}
+
+
+
+
