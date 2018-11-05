cf7612d465da6c8309f438985f4e7109030a54ca
Mrnewliu <15271849112@163.com>
2018-11-06 00:05:16 +0800

    第三十章

--- 12/echo_selectserv.c
+++ 12/echo_selectserv.c
@@ -45,16 +45,77 @@ int main(int argc,char* argv[])
         perror("listen");
         exit(-1);
     }
+    
+    FD_ZERO( &reads );
+    FD_SET( serv_sock, &reads);    
+    fd_max = serv_sock;
+
+    while(1)
+    {
+        cpy_reads = reads;
+        timeout.tv_sec = 5;
+                      
+        timeout.tv_usec = 50000;
+        if(( fd_num = select(fd_max+1, &cpy_reads,0,0, &timeout)) == -1)
+        {
+            break;
+        }
+        if( fd_num == 0)
+        {
+            continue;
+        }
+        
+        for(i =0;i<fd_max+1;i++)
+        {
+            if( FD_ISSET(i,&cpy_reads))
+            {
+                if( i == serv_sock)
+                {
+                    adr_sz = sizeof(clnt_adr);
+                    clnt_sock = accept(serv_sock,(struct sockaddr*)&clnt_adr,&adr_sz);
+                    FD_SET( clnt_sock, &reads);
+                    if( fd_max < clnt_sock)
+                    {
+                        fd_max = clnt_sock;
+                    }
+                    printf("connected client: %d \n", clnt_sock);
+
+                }
+                else
+                {
+                    str_len = read(i, buf, BUF_SIZE);
+                    if(str_len == 0)
+                    {
+                        FD_CLR( i, &reads );
+                        close(i);
+                        printf("closed client: %d \n",i);
+                    }
+                    else
+                    {
+                        write( i, buf, str_len );
+
+                    }
+
+                }
+
+
+            }
+
+        }
+        
+
+
+
+
+
+        
+    }
+
+
+
 
     
-
-
-
-
-
-
-    
-
+    close(serv_sock);
 
 
     
