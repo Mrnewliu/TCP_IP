37dbdfdd5cdd8bb3977e9f93e2d86d029a646d71
Mrnewliu <15271849112@163.com>
2018-11-05 22:02:04 +0800

    dfd

--- 12/echo_selectserv.c
+++ 12/echo_selectserv.c
@@ -12,6 +12,48 @@
 
 int main(int argc,char* argv[])
 {
+    int serv_sock,clnt_sock;
+    struct sockaddr_in  serv_adr,clnt_adr;
+    struct timeval timeout;
+    fd_set reads,cpy_reads;
+    
+    socklen_t adr_sz;
+
+    int fd_max, str_len,fd_num,i;
+    char buf[BUF_SIZE];
+    
+    if( argc != 2)
+    {
+        printf("Usage: %s <prt>\n", argv[0]);
+        exit(1);
+    }
+
+    serv_sock = socket( AF_INET, SOCK_STREAM, 0 );
+    memset(&adr_sz, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_adr.sin_port = htons(atoi(argv[1]));
+
+    if( bind(serv_sock,(struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
+    {
+        perror("bind()\n");
+        exit(-1);
+    }
+
+    
+    
+
+    
+
+
+
+
+
+
+    
+
+
+
     
     return 0;
 }
